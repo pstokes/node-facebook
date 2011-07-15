3f52922397783cd3caf033228bad1360dfafa19f
Patrick Stokes <patrick@buddymedia.com>
2011-07-15 17:31:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+node-facebook
+======================
+A quick and dirty node.js webserver for parsing Facebook's signed request data.
+
+Requirements
+-------------------
+* node.js v0.4.0
+* npm
+
+
+Installation
+-------------------
+	git clone git@github.com:pstokes/node-facebook.git
+	npm install
+	node server.js
---
+++ server.js
@@ -0,0 +1,154 @@
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    qs = require('qs'),
+    fs = require("fs");
+
+var port = 8081;
+
+http.createServer(function(request, response) {
+	if (request.method == 'POST')
+	{
+	    body = '';
+	    request.on('data', function (data) {
+			body += data;
+		});
+
+		request.on('end', function () {
+			post = qs.parse(body);
+
+			var regexS = "[\.](.*$)";
+			var regex = new RegExp( regexS );
+			var results = regex.exec(post.signed_request);
+			var signed_request = null;
+			if (results != null) {
+				r = get_signed_request(results[1]);
+                response.writeHead(200);
+                response.end(JSON.stringify(r));
+                console.log(r)
+			} else {
+                response.writeHead(500);
+                response.end('BAD REQUEST');
+            }
+		});
+	} else {
+		response.writeHead(500);
+		response.end('BAD REQUEST');
+	}
+
+}).listen(port);
+
+function get_signed_request(str)
+{
+    try {
+        return eval( '(' + base64_decode(str) + ')' );
+    } catch(err) {
+        for (i=0; i < (4 - (str.length % 4)); i++) str += '=';        
+        return eval( '(' + base64_decode(str) + ')' );
+    }
+    return false
+}
+
+/* Source: php.js *******************************************************/ 
+function utf8_decode (str_data) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+    // +      input by: Aman Gupta
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Norman "zEh" Fuchs
+    // +   bugfixed by: hitwork
+    // +   bugfixed by: Onno Marsman
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: utf8_decode('Kevin van Zonneveld');
+    // *     returns 1: 'Kevin van Zonneveld'
+    var tmp_arr = [],
+        i = 0,
+        ac = 0,
+        c1 = 0,
+        c2 = 0,
+        c3 = 0;
+
+    str_data += '';
+
+    while (i < str_data.length) {
+        c1 = str_data.charCodeAt(i);
+        if (c1 < 128) {
+            tmp_arr[ac++] = String.fromCharCode(c1);
+            i++;
+        } else if (c1 > 191 && c1 < 224) {
+            c2 = str_data.charCodeAt(i + 1);
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+            i += 2;
+        } else {
+            c2 = str_data.charCodeAt(i + 1);
+            c3 = str_data.charCodeAt(i + 2);
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+    }
+
+    return tmp_arr.join('');
+}
+
+function base64_decode (data) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Tyler Akins (http://rumkin.com)
+    // +   improved by: Thunder.m
+    // +      input by: Aman Gupta
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   bugfixed by: Pellentesque Malesuada
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: utf8_decode
+    // *     example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
+    // *     returns 1: 'Kevin van Zonneveld'
+    // mozilla has this native
+    // - but breaks in 2.0.0.12!
+    //if (typeof this.window['btoa'] == 'function') {
+    //    return btoa(data);
+    //}
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+        ac = 0,
+        dec = "",
+        tmp_arr = [];
+
+    if (!data) {
+        return data;
+    }
+
+    data += '';
+
+    do { // unpack four hexets into three octets using index points in b64
+        h1 = b64.indexOf(data.charAt(i++));
+        h2 = b64.indexOf(data.charAt(i++));
+        h3 = b64.indexOf(data.charAt(i++));
+        h4 = b64.indexOf(data.charAt(i++));
+
+        bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+        o1 = bits >> 16 & 0xff;
+        o2 = bits >> 8 & 0xff;
+        o3 = bits & 0xff;
+
+        if (h3 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1);
+        } else if (h4 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2);
+        } else {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+        }
+    } while (i < data.length);
+
+    dec = tmp_arr.join('');
+    dec = utf8_decode(dec);
+
+    return dec;
+}
+
+console.log("Server running at http://dev.buddymedia.com:" + port + "/");
+
